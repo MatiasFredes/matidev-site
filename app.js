77bde45d256ddc8b82667efde5f0684650f1955b
Matias <mnfredes@gmail.com>
2023-05-06 15:40:25 -0300

    Modifico index.html, agrego resumen

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
-const http = require('http');
-http.createServer(function(req,res){
-	res.write('En camino a ser un full stack engineer');
-	res.end();
-}).listen(3000);
-console.log("server started on port 3000");
+const express = require('express');
+const app = express();
+
+
+app.use(express.static('public'));
+
+// Set up a route for the PDF file
+app.get('/resume', (req, res) => {
+		// Send the PDF file as the response
+		res.sendFile(__dirname + '/public/CV_Matias_Fredes.pdf');
+	});
+
+// Start the server on port 3000
+app.listen(3000, () => {
+	console.log('Server running on http://localhost:3000/');
+});
